749d03dd9449a0f1ba91ca4be2b4398f83bc6ade
igor4ik228 <56679003+igor4ik228@users.noreply.github.com>
2020-04-06 01:31:40 -0700

    рехаэться камера+ выдредактовано розмыр карти

---
+++ Sletta_corona_Edition/Assets/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 2.0F;
+
+    [SerializeField]
+    private Transform target; //за кім буду літати камера
+
+    private void Awake()
+    {
+        if (!target) target = FindObjectOfType<Stella_controller>().transform;
+    }
+    
+    private void Update()
+    {
+        Vector3 position = target.position;     position.z = -20.0F; //вказуємо позицію камери по Z, бо
+                                                                     // якщо би цього б небуло то камра була
+                                                                     // в тій самій плоскості, що і обєкти
+                                                                     // і карта була би програчна
+        
+
+        transform.position = Vector3.Lerp(transform.position, position, speed * Time.deltaTime);
+    }
+    
+}
