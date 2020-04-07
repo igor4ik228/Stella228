1487a53cbfafd67c42318641f5bed9fe6ff47917
igor4ik228 <56679003+igor4ik228@users.noreply.github.com>
2020-04-07 03:41:10 -0700

    f

--- Sletta_corona_Edition/Assets/Demogorgon.cs
+++ Sletta_corona_Edition/Assets/Demogorgon.cs
@@ -7,11 +7,25 @@ public class Demogorgon : Unit
     [SerializeField]
     private float speed = 2.0F;
 
+    private Vector3 direction;
+
     private Bullet bullet;
+
+    private SpriteRenderer sprite;
 
     protected private void Awake()
     {
+        sprite = GetComponentInChildren<SpriteRenderer>();
         bullet = Resources.Load<Bullet>("Bullet");
+    }
+
+    protected private void Start()
+    {
+        direction = transform.right;
+    }
+    protected private void Update()
+    {
+        Move();
     }
 
     protected private void OnTriggerEnter2D(Collider2D collider) //метод для дамага выд пулі (смерті від пулі)
@@ -22,5 +36,14 @@ public class Demogorgon : Unit
         {
             ReceiveDamage();
         }
+    }
+
+    private void Move()//ходьба
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position + transform.up * 0.5F + transform.right * direction.x *0.6F, 0.5F);
+
+        if (colliders.Length > 0) direction *= -1.0F;
+
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
     }
 }
