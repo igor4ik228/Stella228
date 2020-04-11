0c73a620b50356fd298302f405f361da8a771d01
igor4ik228 <56679003+igor4ik228@users.noreply.github.com>
2020-04-11 09:06:13 -0700

    гра ГОТОВА!!!!!!!!!!!!!!!!!!

---
+++ Sletta_corona_Edition/Assets/HeartPlus.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartPlus : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D colider)
+    {
+        Stella_controller stella_controller = colider.GetComponent<Stella_controller>();
+        if (stella_controller)
+        {
+            stella_controller.Lives++;
+            Destroy(gameObject);
+        }
+    }
+}
