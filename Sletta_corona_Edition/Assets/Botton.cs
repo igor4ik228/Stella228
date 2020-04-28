4b6e7004b6affab65cab4ccb984774e8160db5ca
igor4ik228 <56679003+igor4ik228@users.noreply.github.com>
2020-04-28 09:58:59 -0700

    Stella228v9 done
    done

---
+++ Sletta_corona_Edition/Assets/Botton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Botton : MonoBehaviour, IPointerDownHandler
+{
+    /*
+    public GameObject buttons;
+    
+
+
+     public void TapGame()
+     {
+         if (name == "Game")
+         {
+             Application.LoadLevel("SampleScene");
+
+         }
+
+     }
+    public void TapExit()
+    {
+        if (name == "Exit")
+        {
+            Application.Quit();
+        }
+    }
+    */
+
+
+    
+ public void OnPointerDown(PointerEventData eventData)
+ {
+     if (name == "Game")
+     {
+         SceneManager.LoadScene("SampleScene");
+
+     }
+
+     if (name == "Exit")
+     {
+         Application.Quit();
+     }
+ }
+
+}
